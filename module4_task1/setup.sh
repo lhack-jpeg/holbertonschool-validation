8320d529c47caa62dbca4c343b14d8d23a82a345
Luke Hackett <luke-hackett@outlook.com>
2023-01-12 10:58:31 +1100

    add yamllint to setup.sh

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 sudo apt install ./hugo.deb
+sudo apt-get install yamllint
 npm install -g markdownlint-cli
 rm hugo.deb
