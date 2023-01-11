659c5a6fdc8ae8a1f7899cd20ddfd08b125270a3
Luke Hackett <luke-hackett@outlook.com>
2023-01-11 12:50:55 +1100

    add -y flag for npm node install

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 sudo apt install ./hugo.deb
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install -y nodejs
+sudo apt install -y npm
 npm install -g markdownlint-cli
 rm hugo.deb
